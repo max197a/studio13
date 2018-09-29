4868db968ba582d3924336f117502c1495af859b
max197a <max197aa@gmail.com>
2018-09-29 03:23:24 +0300

    fix

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,31 +1,29 @@
-var gulp   = require('gulp');
-var config = require('../config');
+var gulp = require("gulp");
+var config = require("../config");
 
-gulp.task('copy:fonts', function() {
+gulp.task("copy:fonts", function() {
   return gulp
-    .src(config.src.fonts + '/*.{ttf,eot,woff,woff2}')
+    .src(config.src.fonts + "/**/*.{eot,ttf,svg,woff,woff2}")
     .pipe(gulp.dest(config.dest.fonts));
 });
 
-gulp.task('copy:vendor', function() {
+gulp.task("copy:vendor", function() {
   return gulp
-    .src(config.src.vendor + '/**/*.*')
+    .src(config.src.vendor + "/**/*.*")
     .pipe(gulp.dest(config.dest.vendor));
 });
 
-gulp.task('copy:rootfiles', function() {
-  return gulp
-    .src(config.src.root + '/*.*')
-    .pipe(gulp.dest(config.dest.root));
+gulp.task("copy:rootfiles", function() {
+  return gulp.src(config.src.root + "/*.*").pipe(gulp.dest(config.dest.root));
 });
 
-gulp.task('copy', [
+gulp.task("copy", [
   // 'copy:rootfiles',
-  'copy:vendor',
-  'copy:fonts'
+  "copy:vendor",
+  "copy:fonts"
 ]);
 
-gulp.task('copy:watch', function() {
-  gulp.watch(config.src.vendor + '/**/*.*', ['copy:vendor']);
-  gulp.watch(config.src.root + '/*.*', ['copy:rootfiles']);
+gulp.task("copy:watch", function() {
+  gulp.watch(config.src.vendor + "/**/*.*", ["copy:vendor"]);
+  gulp.watch(config.src.root + "/*.*", ["copy:rootfiles"]);
 });
